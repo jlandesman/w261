a2f582f8bbea8b2980b756a6713a8b01fb980326
Koding <jlandesman@gmail.com>
2016-08-29 22:52:11 +0000

    Uploading week 1 quiz

--- Quizzes/Week 1/mapper.py
+++ Quizzes/Week 1/mapper.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python
 import sys
 import re
-count = []
+count = 0
 WORD_RE = re.compile(r"[\w']+")
 filename = sys.argv[2]
 findword = sys.argv[1]
+
 with open (filename, "r") as myfile:
-    print myfile
+    for line in myfile.readlines():
+        words = line.lower().split()
+        num_matches = [1 for x in words if x.strip('!@#$%^&*,?/.') == findword.lower()]
+        count += sum(num_matches)
+
+print(count)
